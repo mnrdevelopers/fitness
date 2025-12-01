1c60b9f25b1477669f6ef3f0ca64da80fef87449
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-12-01 09:06:56 +0530

    Change Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD71QYDbPEETn-24nT45h4AdD_W6_inyWU",
-  authDomain: "fitness-healthylifestyle.firebaseapp.com",
-  projectId: "fitness-healthylifestyle",
-  storageBucket: "fitness-healthylifestyle.appspot.com",
-  messagingSenderId: "762783082047",
-  appId: "1:762783082047:web:04f79dfe0ee4b7ae6c7a8a"
+  apiKey: "AIzaSyBp1yyC1IF_rmOWwFdZRcbcsCHNbJ3Sdro",
+  authDomain: "mnr-devops-2e97d.firebaseapp.com",
+  projectId: "mnr-devops-2e97d",
+  storageBucket: "mnr-devops-2e97d.firebasestorage.app",
+  messagingSenderId: "464172080556",
+  appId: "1:464172080556:web:97cecddd2e236f387aee09",
+  measurementId: "G-9SXTYCDF9W"
 };
 
 // Initialize Firebase
